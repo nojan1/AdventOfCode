17558516a6fa06bc57b1d66a3f83593642695ac5
Niklas Hedlund <nojan1989@gmail.com>
2015-12-10 21:51:43 +0100

    Day5 part 1

---
+++ Day5/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var vowels = "aeiou".ToCharArray();
+            var bad = new string[] { "ab", "cd", "pq", "xy" };
+            int nice = 0;
+
+            foreach (var str in File.ReadAllLines("Input.txt"))
+            {
+                if(str.ToCharArray().Count(c => vowels.Contains(c)) >= 3 
+                    && LetterRepeats(str) 
+                    && !bad.Any(b => str.Contains(b))){
+
+                    nice++;
+                }
+            }
+
+            Console.WriteLine("{0} strings are nice", nice);
+            Console.Read();
+        }
+
+        static bool LetterRepeats(string text)
+        {
+            foreach(char letter in text)
+            {
+                string search = letter.ToString() + letter.ToString();
+                if (text.Contains(search))
+                {
+                    return true;
+                }                
+            }
+
+            return false;
+        }
+    }
+}
