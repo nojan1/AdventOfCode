5cce6e0a4ec4ac9a5251a7825a5b6d6f7eec8e3f
Niklas Hedlund <nojan1989@gmail.com>
2016-12-06 13:00:31 +0100

    Day 6

---
+++ 2016/Day6/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = File.ReadAllLines("input.txt").Select(t => t.Trim()).ToArray();
+
+            var message = "";
+            for(int x = 0; x < input[0].Length; x++)
+            {
+                var dict = new Dictionary<char, int>();
+                for(int y = 0; y < input.Length; y++)
+                {
+                    if (!dict.ContainsKey(input[y][x]))
+                    {
+                        dict[input[y][x]] = 1;
+                    }
+                    else
+                    {
+                        dict[input[y][x]]++;
+                    }
+                }
+
+               message += dict.FirstOrDefault(d => d.Value == dict.Values.Min()).Key;
+            }
+
+            Console.WriteLine($"The message is: '{message}'");
+        }
+    }
+}
