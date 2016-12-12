e34c25a8a1254360c86200fd8552859f8eb5b950
Niklas Hedlund <nojan1989@gmail.com>
2016-12-12 09:05:28 +0100

    2015 Day 8 partial

---
+++ 2015/Day8/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Day8
+{
+    class Program
+    {
+        static string CustomEscape(string data)
+        {
+            data = Regex.Replace(data, @"\\", @"\");
+            data = Regex.Replace(data, @"\""", @"""");
+            return Regex.Replace(data, @"(\\x\d{2})", "-");      
+        }
+
+        static void Main(string[] args)
+        {
+            int countEscaped = 0, countUnEscaped = 0;
+
+            foreach(var line in File.ReadAllLines("input.txt").Select(t => t.Trim())){
+                countUnEscaped += line.Length;
+                countEscaped += CustomEscape(line).Length - 2; 
+            }
+
+            Console.WriteLine($"The diff is {countUnEscaped - countEscaped}");
+        }
+    }
+}
