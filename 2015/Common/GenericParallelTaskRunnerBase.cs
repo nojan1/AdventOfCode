c3f42fb7ab3fd1d30855d4fa3b5ecccf224177c6
Niklas Hedlund <nojan1989@gmail.com>
2017-11-29 22:59:34 +0200

    Added common project with parallel task runner

---
+++ 2015/Common/GenericParallelTaskRunnerBase.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    public abstract class GenericParallelTaskRunnerBase<TParameter, TReturnValue>
+    {
+        private int _maxWorkers;
+        private List<Task> _taskList = new List<Task>();
+
+        public bool RunComplete {get; protected set;}
+
+        public GenericParallelTaskRunnerBase(int maxWorkers)
+        {
+            _maxWorkers = maxWorkers;
+        }
+
+        public void RunToEnd()
+        {
+            while (!RunComplete)
+            {
+                while(_taskList.Count < _maxWorkers)
+                {
+                    var parameter = CreateTaskParameter();
+                    if (parameter == null)
+                        break;
+
+                    var task = Task.Run<TReturnValue>(() =>
+                    {
+                        return Worker(parameter);
+                    }).ContinueWith(result =>
+                    {
+                        OnTaskFinished(parameter, result.Result);
+                    });
+
+                    _taskList.Add(task);
+                }
+
+                if(!_taskList.Any())
+                {
+                    RunComplete = true;
+                    return;
+                }
+
+                int taskIndex = Task.WaitAny(_taskList.ToArray());
+                var finishedTask = _taskList[taskIndex];
+
+                _taskList.Remove(finishedTask);
+            }
+        }
+
+        protected abstract TParameter CreateTaskParameter();
+        protected abstract TReturnValue Worker(TParameter parameter);
+        protected abstract void OnTaskFinished(TParameter taskParameter, TReturnValue taskReturnValue);
+    }
+}
