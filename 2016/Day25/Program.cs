4ac04c74e8c82ecefd2fc96a930c4526df6574cd
Niklas Hedlund <nojan1989@gmail.com>
2017-11-29 23:25:36 +0200

    Day25 in parallel

--- 2016/Day25/Program.cs
+++ 2016/Day25/Program.cs
@@ -1,4 +1,5 @@
-﻿using Day12;
+﻿using Common;
+using Day12;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -8,17 +9,91 @@ using System.Threading.Tasks;
 
 namespace Day25
 {
-    class Program
+    class Runner : GenericParallelTaskRunnerBase<int, bool>
     {
-        static void Main(string[] args)
+        public int CorrectValue { get; private set; }
+
+        private string[] _instructions;
+        private int a = 7897;
+
+        public Runner(string[] instructions) : base(4)
+        {
+            _instructions = instructions;
+        }
+
+        protected override int CreateTaskParameter()
+        {
+            return a++;
+        }
+
+        protected override void OnTaskFinished(int taskParameter, bool taskReturnValue)
+        {
+            if (taskReturnValue)
+            {
+                CorrectValue = taskParameter;
+                RunComplete = true;
+            }
+        }
+
+        protected override bool Worker(int a)
         {
             var outputBuffer = new List<int>();
-            var cpu = new BunnyPU(File.ReadAllLines("assembunny.txt").Select(t => t.Trim()).ToArray());
+            var cpu = new BunnyPU(_instructions);
 
             cpu.OnOutput += x =>
             {
                 outputBuffer.Insert(0, x);
             };
+
+            int pointer = 0;
+
+            outputBuffer.Clear();
+            pointer = 0;
+            cpu.GetRegister("a").Value = a;
+
+            while (outputBuffer.Count < 20)
+            {
+                pointer = cpu.RunSingleInstruction(pointer);
+                if (pointer == -1)
+                    break;
+            }
+
+            return BufferContainsRepeatingPattern(outputBuffer);
+        }
+
+        private bool BufferContainsRepeatingPattern(List<int> outputBuffer)
+        {
+            for (int i = 0; i < outputBuffer.Count; i++)
+            {
+                if (i + 4 > outputBuffer.Count - 1)
+                {
+                    return false;
+                }
+
+                if (outputBuffer[i + 0] != 0 ||
+                   outputBuffer[i + 1] != 1 ||
+                   outputBuffer[i + 2] != 0 ||
+                   outputBuffer[i + 3] != 1)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var outputBuffer = new List<int>();
+            //var cpu = new BunnyPU(File.ReadAllLines("assembunny.txt").Select(t => t.Trim()).ToArray());
+
+            //cpu.OnOutput += x =>
+            //{
+            //    outputBuffer.Insert(0, x);
+            //};
 
             //int a = 7897, pointer = 0;
             //while (true)
@@ -40,14 +115,18 @@ namespace Day25
             //    a++;
             //}
 
-            bool valueFound = true;
-            Parallel.For(7897, int.MaxValue, a =>
-            {
-                int pointer = 0;
+            //bool valueFound = true;
+            //Parallel.For(7897, int.MaxValue, a =>
+            //{
+            //    int pointer = 0;
 
-            });
+            //});
 
-            Console.WriteLine($"The magic value is {a}");
+            var instructions = File.ReadAllLines("assembunny.txt").Select(t => t.Trim()).ToArray();
+            var runner = new Runner(instructions);
+            runner.RunToEnd();
+
+            Console.WriteLine($"The magic value is {runner.CorrectValue}");
         }
 
         private static bool BufferContainsRepeatingPattern(List<int> outputBuffer)
