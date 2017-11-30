b6af812f136fae806cd7327cee130bbf3f62a28e
Niklas Hedlund <nojan1989@gmail.com>
2017-11-30 14:02:48 +0100

    Fixed day25

--- 2016/Day25/Program.cs
+++ 2016/Day25/Program.cs
@@ -16,7 +16,7 @@ namespace Day25
         public int CorrectValue { get; private set; }
 
         private string[] _instructions;
-        private int a = 7897;
+        private int a = 1;
 
         public Runner(string[] instructions) : base(NUM_THREADS)
         {
@@ -44,7 +44,7 @@ namespace Day25
 
             cpu.OnOutput += x =>
             {
-                outputBuffer.Insert(0, x);
+                outputBuffer.Add(x);
             };
 
             int pointer = 0;
@@ -65,13 +65,8 @@ namespace Day25
 
         private bool BufferContainsRepeatingPattern(List<int> outputBuffer)
         {
-            for (int i = 0; i < outputBuffer.Count; i++)
+            for (int i = 0; i < outputBuffer.Count; i+= 4)
             {
-                if (i + 4 > outputBuffer.Count - 1)
-                {
-                    return false;
-                }
-
                 if (outputBuffer[i + 0] != 0 ||
                    outputBuffer[i + 1] != 1 ||
                    outputBuffer[i + 2] != 0 ||
@@ -89,67 +84,11 @@ namespace Day25
     {
         static void Main(string[] args)
         {
-            //var outputBuffer = new List<int>();
-            //var cpu = new BunnyPU(File.ReadAllLines("assembunny.txt").Select(t => t.Trim()).ToArray());
-
-            //cpu.OnOutput += x =>
-            //{
-            //    outputBuffer.Insert(0, x);
-            //};
-
-            //int a = 7897, pointer = 0;
-            //while (true)
-            //{
-            //    outputBuffer.Clear();
-            //    pointer = 0;
-            //    cpu.GetRegister("a").Value = a;
-
-            //    while (outputBuffer.Count < 20)
-            //    {
-            //        pointer = cpu.RunSingleInstruction(pointer);
-            //        if (pointer == -1)
-            //            break;
-            //    }
-
-            //    if (BufferContainsRepeatingPattern(outputBuffer))
-            //        break;
-
-            //    a++;
-            //}
-
-            //bool valueFound = true;
-            //Parallel.For(7897, int.MaxValue, a =>
-            //{
-            //    int pointer = 0;
-
-            //});
-
             var instructions = File.ReadAllLines("assembunny.txt").Select(t => t.Trim()).ToArray();
             var runner = new Runner(instructions);
             runner.RunToEnd();
 
             Console.WriteLine($"The magic value is {runner.CorrectValue}");
-        }
-
-        private static bool BufferContainsRepeatingPattern(List<int> outputBuffer)
-        {
-            for (int i = 0; i < outputBuffer.Count; i++)
-            {
-                if (i + 4 > outputBuffer.Count - 1)
-                {
-                    return false;
-                }
-
-                if (outputBuffer[i + 0] != 0 ||
-                   outputBuffer[i + 1] != 1 ||
-                   outputBuffer[i + 2] != 0 ||
-                   outputBuffer[i + 3] != 1)
-                {
-                    return false;
-                }
-            }
-
-            return true;
         }
     }
 }
