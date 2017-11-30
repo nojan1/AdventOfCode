0db66ad1841f8c0f59031f61461ab7f94ce97178
Niklas Hedlund <nojan1989@gmail.com>
2017-11-30 16:29:37 +0100

    Day22

---
+++ 2015/Day22/Program.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day22
+{
+    
+    class Player
+    {
+        public int Hitpoints { get; set; }
+        public int Mana { get; set; }
+        public int Shield { get; set; }
+
+        public ICollection<Spell> Spells { get; set; }
+
+        public Player()
+        {
+            Reset();
+        }
+
+        public void ApplyEffects(Boss boss)
+        {
+            foreach (var spell in Spells.Where(s => s.EffectActive))
+                spell.ApplyEffect(boss);
+        }
+
+        public void TakeDamage(int damage)
+        {
+            Hitpoints -= Math.Max(1, damage - Shield);
+        }
+
+        public void Reset()
+        {
+            Mana = 500;
+            Hitpoints = 50;
+            Shield = 0;
+
+            Spells = new Spell[]
+            {
+                new MagicMissile(this),
+                new Drain(this),
+                new Shield(this),
+                new Poison(this),
+                new Recharge(this)
+            };
+        }
+    }
+
+    class Boss
+    {
+        public int Hitpoints { get; set; }
+        public int Damage { get; private set; }
+
+        private int _startingHitpoints;
+
+        public Boss(int hitpoints, int damage)
+        {
+            _startingHitpoints = hitpoints;
+            Damage = damage;
+
+            Reset();
+        }
+
+        public void Reset()
+        {
+            Hitpoints = _startingHitpoints;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var boss = new Boss(55, 8);
+            var player = new Player();
+
+            var rnd = new Random();
+            int lowestManaUsage = int.MaxValue;
+
+            while (true)
+            {
+                int manaUsed = 0;
+                var spellsUsed = new List<string>();
+
+                player.Reset();
+                boss.Reset();
+
+                while (boss.Hitpoints > 0 && player.Hitpoints > 0)
+                {
+                    //Players turn
+                    player.Hitpoints -= 1;
+                    if (player.Hitpoints <= 0)
+                        break;
+
+                    player.ApplyEffects(boss);
+
+                    if (player.Mana < player.Spells.Min(s => s.Cost))
+                    {
+                        player.Hitpoints = 0;
+                        break;
+                    }
+
+                    var castableSpells = player.Spells.Where(s => s.CanCast).ToArray();
+                    var spellToCast = castableSpells[rnd.Next(0, castableSpells.Length)];
+
+                    spellsUsed.Add(spellToCast.Name);
+                    manaUsed += spellToCast.Cost;
+                    spellToCast.Cast(boss);
+
+                    //Boss turn
+                    player.ApplyEffects(boss);
+                    player.TakeDamage(boss.Damage);
+                }
+
+                if(boss.Hitpoints <= 0 && manaUsed < lowestManaUsage)
+                {
+                    lowestManaUsage = manaUsed;
+
+                    Console.Clear();
+                    Console.WriteLine($"Mana used: {lowestManaUsage}");
+                    Console.WriteLine(string.Join(" -> ", spellsUsed));
+                }
+            }
+        }
+    }
+}
---
+++ 2015/Day22/Spell.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day22
+{
+    class MagicMissile : Spell
+    {
+        public MagicMissile(Player player) : base("Magic Missile", 53, false, player)
+        {
+        }
+
+        protected override void CastInner(Boss boss)
+        {
+            boss.Hitpoints -= 4;
+        }
+    }
+
+    class Drain : Spell
+    {
+        public Drain(Player player) : base("Drain", 73, false, player)
+        {
+        }
+
+        protected override void CastInner(Boss boss)
+        {
+            boss.Hitpoints -= 2;
+            _player.Hitpoints += 2;
+        }
+    }
+
+    class Shield : Spell
+    {
+        public Shield(Player player) : base("Shield", 113, true, player)
+        {
+        }
+
+        protected override void CastInner(Boss boss)
+        {
+            EffectActive = true;
+            EffectCounter = 0;
+
+            _player.Shield += 7;
+        }
+
+        public override void ApplyEffect(Boss boss)
+        {
+            base.ApplyEffect(boss);
+
+            if(++EffectCounter == 6)
+            {
+                EffectActive = false;
+                _player.Shield -= 7;
+            }
+        }
+    }
+
+    class Poison : Spell
+    {
+        public Poison(Player player) : base("Poison", 173, true, player)
+        {
+        }
+
+        protected override void CastInner(Boss boss)
+        {
+
+            EffectActive = true;
+            EffectCounter = 0;
+        }
+
+        public override void ApplyEffect(Boss boss)
+        {
+            base.ApplyEffect(boss);
+
+            boss.Hitpoints -= 3;
+
+            if (++EffectCounter == 6)
+            {
+                EffectActive = false;
+            }
+        }
+    }
+
+    class Recharge : Spell
+    {
+        public Recharge(Player player) : base("Recharge", 229, true, player)
+        {
+        }
+
+        protected override void CastInner(Boss boss)
+        {
+
+            EffectActive = true;
+            EffectCounter = 0;
+        }
+
+        public override void ApplyEffect(Boss boss)
+        {
+            base.ApplyEffect(boss);
+
+            _player.Mana += 101;
+
+            if (++EffectCounter == 5)
+            {
+                EffectActive = false;
+            }
+        }
+    }
+
+    abstract class Spell
+    {
+        public string Name { get; private set; }
+        public int Cost { get; private set; }
+
+        public bool CanCast { get => _player.Mana >= Cost && (!HasEffect || (HasEffect && !EffectActive)); }
+
+        public bool HasEffect { get; protected set; }
+        public bool EffectActive { get; protected set; } = false;
+        public int EffectCounter { get; protected set; } = 0;
+
+        protected Player _player;
+
+        public Spell(string name, int cost, bool hasEffect, Player player)
+        {
+            Name = name;
+            Cost = cost;
+            HasEffect = hasEffect;
+
+            _player = player;
+        }
+
+        public void Cast(Boss boss)
+        {
+            _player.Mana -= Cost;
+            CastInner(boss);
+        }
+        
+        public virtual void ApplyEffect(Boss boss) { }
+
+        protected abstract void CastInner(Boss boss);
+    }
+}
