8b09798f3aad6739f892337f9d204c78576e5a1e
Niklas Hedlund <nojan1989@gmail.com>
2017-12-01 08:45:21 +0100

    Day1 2017

---
+++ 2015/Day24/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day24
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var weights = File.ReadAllLines("input.txt").Select(x => Convert.ToInt32(x.Trim())).ToList();
+
+            var weightList = weights.OrderBy(x => x).ToList();
+            var groups = new List<int>[] { new List<int>(), new List<int>(), new List<int>() };
+
+            while (weightList.Any())
+            {
+                groups[0].Add(weightList.Last());
+                weightList.RemoveAt(weightList.Count - 1);
+
+
+            }
+
+        }
+    }
+}
---
+++ 2017/Day1/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day1
+{
+    class Program
+    {
+        static int Part1(string input)
+        {
+            int captcha = 0;
+            for (int i = 0; i < input.Length; i++)
+            {
+                var next = i == input.Length - 1 ? 0 : i + 1;
+
+                if (input[i] == input[next])
+                {
+                    captcha += Convert.ToInt32(input[i].ToString());
+                }
+            }
+
+            return captcha;
+        }
+
+        private static int Part2(string input)
+        {
+            int captcha = 0;
+            for (int i = 0; i < input.Length; i++)
+            {
+                var next = i + (input.Length / 2);
+                if(next > input.Length - 1)
+                {
+                    next -= input.Length;
+                }
+
+                if (input[i] == input[next])
+                {
+                    captcha += Convert.ToInt32(input[i].ToString());
+                }
+            }
+
+            return captcha;
+        }
+
+        static void Main(string[] args)
+        {
+            var input = File.ReadAllText("input.txt");
+
+            //var captcha1 = Part1(input);
+            var captcha = Part2(input);
+
+            Console.WriteLine($"The captcha is: {captcha}");
+        }
+    }
+}
