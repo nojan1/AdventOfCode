4f813eb690c85aa4162795d31eeda7f87593e098
Niklas Hedlund <nojan1989@gmail.com>
2017-12-12 13:03:43 +0100

    Day 25 part 1

--- 2015/Day25/Program.cs
+++ 2015/Day25/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,96 +7,79 @@ using System.Threading.Tasks;
 
 namespace Day25
 {
-    class CodeGrid
+    class Code
     {
-        private long[,] _grid;
-        private int _sideSize;
-        private (int row, int column) _lastGeneratedCoord;
-        private int _maxRow;
-        private int _maxColumn;
+        public int Row { get; set; }
+        public int Column { get; set; }
+        public ulong Value { get; set; }
+    }
 
-        private List<(int row, int column)> _coords = new List<(int row, int column)>();
+    class CodeEnumerator : IEnumerator<Code>
+    {
+        private Code _currentCode;
 
-        public CodeGrid(int sideSize)
+        public CodeEnumerator()
         {
-            _sideSize = sideSize;
-            _grid = new long[sideSize, sideSize];
-
-            _grid[1, 1] = 20151125;
-            _grid[2, 1] = 31916031;
-            _grid[3, 1] = 16080970;
-            _grid[4, 1] = 24592653;
-            _grid[5, 1] = 77061;
-            _grid[6, 1] = 33071741;
-
-            _grid[1, 2] = 18749137;
-            _grid[2, 2] = 21629792;
-            _grid[3, 2] = 8057251;
-            _grid[4, 2] = 32451966;
-            _grid[5, 2] = 17552253;
-
-            _grid[1, 3] = 17289845;
-            _grid[2, 3] = 16929656;
-            _grid[3, 3] = 1601130;
-            _grid[4, 3] = 21345942;
-
-            _grid[1, 4] = 30943339;
-            _grid[2, 4] = 7726640;
-            _grid[3, 4] = 7981243;
-
-            _grid[1, 5] = 10071777;
-            _grid[2, 5] = 15514188;
-
-            _grid[1, 6] = 33511524;
-
-            _maxRow = 6;
-            _maxColumn = 6;
-
-            _lastGeneratedCoord = (1, 6);
-
-            Fill();
-            
+            Reset();
         }
 
-        private void Fill()
+        public Code Current => _currentCode;
+
+        object IEnumerator.Current => _currentCode;
+
+        public void Dispose() { }
+
+        public bool MoveNext()
         {
-            while(_lastGeneratedCoord.column < _sideSize - 1)
+            if (_currentCode == null)
             {
-                var nextCoord = GetNextCoord(_lastGeneratedCoord.row, _lastGeneratedCoord.column);
-                var lastValue = GetValue(_lastGeneratedCoord.row, _lastGeneratedCoord.column);
-
-                var newValue = (lastValue * 252533) % 33554393;
-
-                _grid[nextCoord.row, nextCoord.column] = newValue;
-                _lastGeneratedCoord = nextCoord;
-
-                if (nextCoord.row > _maxRow)
-                    _maxRow = nextCoord.row;
-
-                if (nextCoord.column > _maxColumn)
-                    _maxColumn = nextCoord.column;
-
-                _coords.Add(_lastGeneratedCoord);
+                _currentCode = new Code
+                {
+                    Row = 1,
+                    Column = 1,
+                    Value = 20151125
+                };
             }
-        }
-
-        public long GetValue(int row, int column)
-        {
-            return _grid[row, column];
-        }
-
-        private (int row, int column) GetNextCoord(int row, int column)
-        {
-            int newRow = --row;
-            int newColumn = ++column;
-
-            if(newRow < 1)
+            else
             {
-                newColumn = 1;
-                newRow = _maxRow + 1;
+                int newColumn = ++_currentCode.Column;
+                int newRow = --_currentCode.Row;
+
+                ulong newValue = (_currentCode.Value * 252533) % 33554393;
+
+                if (newRow < 1)
+                {
+                    newRow = newColumn;
+                    newColumn = 1;
+                }
+
+                _currentCode = new Code
+                {
+                    Row = newRow,
+                    Column = newColumn,
+                    Value = newValue
+                };
             }
 
-            return (newRow, newColumn);
+            return true;
+        }
+
+        public void Reset()
+        {
+            _currentCode = null;
+        }
+    }
+
+    class CodeEnumberable : IEnumerable<Code>
+    {
+        public IEnumerator<Code> GetEnumerator()
+        {
+            return new CodeEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return new CodeEnumerator();
         }
     }
 
@@ -103,17 +87,14 @@ namespace Day25
     {
         static void Main(string[] args)
         {
-            var input = "To continue, please consult the code grid in the manual.  Enter the code at row 2947, column 3029.";
+            //To continue, please consult the code grid in the manual.  Enter the code at row 2947, column 3029.
             var targetRow = 2947;
-            var targetColumn = 3020;
+            var targetColumn = 3029;
 
-            var grid = new CodeGrid(6000);
-            var code = grid.GetValue(targetRow, targetColumn);
+            var codeEnumerable = new CodeEnumberable();
+            var code = codeEnumerable.First(x => x.Row == targetRow && x.Column == targetColumn).Value;
 
-            if (code >= 28030537)
-                Console.ForegroundColor = ConsoleColor.Red;
-
-            Console.WriteLine($"The code is: {code}");
+            Console.WriteLine($"The code is {code}");
         }
     }
 }
