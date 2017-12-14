db84b30b2f829dfe491104341762c61b845e47fe
Niklas Hedlund <nojan1989@gmail.com>
2017-12-14 13:38:25 +0100

    Day14 complete

--- 2017/Day10/Program.cs
+++ 2017/Day10/Program.cs
@@ -7,9 +7,9 @@ using System.Threading.Tasks;
 
 namespace Day10
 {
-    class Program
+    public class KnotHasher
     {
-        static byte[] CalculateSparseHash(IEnumerable<byte> lengths, int numRounds)
+        internal static byte[] CalculateSparseHash(IEnumerable<byte> lengths, int numRounds)
         {
             var numbers = new CircularList<byte>(Enumerable.Range(0, 256).Select(x => (byte)x));
             int currentPosition = 0;
@@ -20,7 +20,7 @@ namespace Day10
                 foreach (var length in lengths)
                 {
                     var workingCopy = new List<byte>();
-                    for(byte i = 0; i < length; i++)
+                    for (byte i = 0; i < length; i++)
                     {
                         workingCopy.Add(numbers[currentPosition + i]);
                     }
@@ -37,28 +37,37 @@ namespace Day10
             return numbers.ToArray();
         }
 
-        static string CalculateDenseHash(IEnumerable<byte> lengths)
+        public static string CalculateDenseHash(string value)
+        {
+            var inputBytes = value.Select(x => (byte)x).ToList();
+            inputBytes.AddRange(new byte[] { 17, 31, 73, 47, 23 });
+
+            return CalculateDenseHash(inputBytes);
+        }
+
+        public static string CalculateDenseHash(IEnumerable<byte> lengths)
         {
             var sparseHash = CalculateSparseHash(lengths, 64);
             var blocks = Enumerable.Range(0, 16).Select(i => sparseHash.Skip(i * 16).Take(16).ToArray()).ToArray();
-            var blockValues = blocks.Select(x => x.Aggregate((tot, self) => (byte)(tot ^ self) )).ToArray();
+            var blockValues = blocks.Select(x => x.Aggregate((tot, self) => (byte)(tot ^ self))).ToArray();
 
             return string.Concat(blockValues.Select(x => x.ToString("x2")));
         }
+    }
 
+    class Program
+    {
         static void Main(string[] args)
         {
             var input = "187,254,0,81,169,219,1,190,19,102,255,56,46,32,2,216";
 
             var lengthInts = input.Split(',').Select(i => Convert.ToByte(i)).ToArray();
-            var part1SparseHash = CalculateSparseHash(lengthInts, 1);
+            var part1SparseHash = KnotHasher.CalculateSparseHash(lengthInts, 1);
             var part1Answer = (int)part1SparseHash[0] * (int)part1SparseHash[1];
 
             Console.WriteLine($"The answer to part1 is {part1Answer}");
 
-            var lengthAsciiBytes = input.ToCharArray().Select(x => (byte)x).ToList();
-            lengthAsciiBytes.AddRange(new byte[] { 17, 31, 73, 47, 23 });
-            var part2DenseHash = CalculateDenseHash(lengthAsciiBytes);
+            var part2DenseHash = KnotHasher.CalculateDenseHash(input);
 
             Console.WriteLine($"Part 2 dense hash is: '{part2DenseHash}'");
         }
---
+++ 2017/Day14/Program.cs
@@ -0,0 +1,87 @@
+﻿using Common;
+using Day10;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day14
+{
+    class Square
+    {
+        public (int Row, int Column) Position { get; set; }
+
+        public List<Square> RegionMembers { get; private set; } = new List<Square>();
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = "hxtvlmkl";
+
+            var rowHashes = Enumerable.Range(0, 128)
+                .Select(x =>
+                {
+                    var value = $"{input}-{x}";
+                    var hash = KnotHasher.CalculateDenseHash(value);
+
+                    return String.Concat(hash.Select(c =>
+                    {
+                        var number = Convert.ToUInt32(c.ToString(), 16);
+                        var binary = Convert.ToString(number, 2).PadLeft(4, '0');
+                        return binary;
+                    }));
+                })
+                .ToArray();
+
+            var usageCount = String.Concat(rowHashes).Count(c => c == '1');
+            Console.WriteLine($"There are {usageCount} squares used");
+
+            var coords = new List<(int Row, int Column)>();
+            for (int row = 0; row < rowHashes.Length; row++)
+            {
+                for (int column = 0; column < rowHashes[0].Length; column++)
+                {
+                    if (rowHashes[row][column] == '1')
+                        coords.Add((row, column));
+
+                }
+            }
+
+            var regions = new List<List<(int Row, int Column)>>();
+            while (coords.Any())
+            {
+                var regionCoords = new List<(int Row, int Column)>();
+                BuildNeighbours((coords.First().Row, coords.First().Column), coords, regionCoords);
+                regions.Add(regionCoords);
+            }
+            
+            Console.WriteLine($"There are {regions.Count} regions"); 
+        }
+
+        private static void BuildNeighbours((int Row, int Column) centerAround, List<(int Row, int Column)> coords, List<(int Row, int Column)> regionCoords)
+        {
+            if (centerAround.Row < 0 || centerAround.Column < 0 || centerAround.Row > 127 || centerAround.Column > 127)
+                return;
+
+            if (regionCoords.Contains(centerAround))
+                return;
+
+            if (coords.Any(c => c.Row == centerAround.Row && c.Column == centerAround.Column)) { 
+                regionCoords.Add(centerAround);
+                coords.RemoveAll(c => c.Row == centerAround.Row && c.Column == centerAround.Column);
+            }
+            else
+            {
+                return;
+            }
+
+            BuildNeighbours((centerAround.Row - 1, centerAround.Column), coords, regionCoords);
+            BuildNeighbours((centerAround.Row, centerAround.Column + 1), coords, regionCoords);
+            BuildNeighbours((centerAround.Row + 1, centerAround.Column), coords, regionCoords);
+            BuildNeighbours((centerAround.Row, centerAround.Column - 1), coords, regionCoords);
+        }
+    }
+}
