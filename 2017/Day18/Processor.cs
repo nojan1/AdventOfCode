cca1d14640306ef4aab5b5121c500f0fe939bf73
Niklas Hedlund <nojan1989@gmail.com>
2017-12-18 16:29:02 +0100

    Day18

---
+++ 2017/Day18/Processor.cs
@@ -0,0 +1,135 @@
+﻿using Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day18
+{
+    public class Register
+    {
+        public string Name { get; set; }
+        public long Value { get; set; }
+    }
+
+    public class Processor
+    {
+        private DayPart _dayPart;
+        private string[] _instructions;
+
+        public List<Register> Registers { get; private set; } = new List<Register>();
+        public Queue<long> InputQueue { get; private set; } = new Queue<long>();
+
+        public event Action<long> OnRecieve = delegate { };
+        public event Action<long> OnSend = delegate { };
+
+        public Processor(string[] instructions, DayPart dayPart)
+        {
+            _instructions = instructions;
+            _dayPart = dayPart;
+        }
+
+        public Register GetRegister(string name)
+        {
+            var register = Registers.FirstOrDefault(r => r.Name == name);
+            if (register == null)
+            {
+                register = new Register { Name = name };
+                Registers.Add(register);
+            }
+
+            return register;
+        }
+
+        public int RunSingleInstruction(int instructionPointer)
+        {
+            if (instructionPointer > _instructions.Count() - 1)
+            {
+                return -1;
+            }
+
+            var instructionParts = _instructions[instructionPointer].Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries).Select(t => t.ToLower()).ToArray();
+
+            if (instructionParts[0].StartsWith("//"))
+                return instructionPointer;
+
+            switch (instructionParts[0])
+            {
+                case "set":
+                    GetRegister(instructionParts[1]).Value = GetRawOrRegisterValue(instructionParts[2]);
+
+                    break;
+                case "add":
+                    GetRegister(instructionParts[1]).Value += GetRawOrRegisterValue(instructionParts[2]);
+
+                    break;
+                case "jgz":
+                    var conditionValue = GetRawOrRegisterValue(instructionParts[1]);
+                    if (conditionValue > 0)
+                    {
+                        var pointerChange = GetRawOrRegisterValue(instructionParts[2]);
+                        instructionPointer += (int)pointerChange - 1;
+                    }
+
+                    break;
+                case "mul":
+                    GetRegister(instructionParts[1]).Value = GetRawOrRegisterValue(instructionParts[1]) * GetRawOrRegisterValue(instructionParts[2]);
+
+                    break;
+                case "mod":
+                    GetRegister(instructionParts[1]).Value = GetRawOrRegisterValue(instructionParts[1]) % GetRawOrRegisterValue(instructionParts[2]);
+
+                    break;
+                case "rcv":
+                    if (_dayPart == DayPart.One)
+                    {
+                        var value = GetRawOrRegisterValue(instructionParts[1]);
+                        if (value != 0)
+                        {
+                            OnRecieve(value);
+                        }
+                    }
+                    else
+                    {
+                        if (!InputQueue.Any())
+                            return instructionPointer;
+
+                        GetRegister(instructionParts[1]).Value = InputQueue.Dequeue();
+                    }
+
+                    break;
+                case "snd":
+                    OnSend(GetRawOrRegisterValue(instructionParts[1]));
+
+                    break;
+                case "nop":
+                    //Troloolol
+                    break;
+                default:
+                    throw new Exception("Unsupported instruction: " + _instructions[instructionPointer]);
+            }
+
+            return instructionPointer + 1;
+        }
+
+        public void RunToEnd()
+        {
+            for (int instructionPointer = 0; instructionPointer < _instructions.Count(); instructionPointer++)
+            {
+                instructionPointer = RunSingleInstruction(instructionPointer) - 1;
+            }
+        }
+
+        private long GetRawOrRegisterValue(string x)
+        {
+            long value;
+            if (!long.TryParse(x, out value))
+            {
+                value = GetRegister(x).Value;
+            }
+
+            return value;
+        }
+    }
+}
---
+++ 2017/Day18/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day18
+{
+    class Program
+    {
+        static long A(string[] instructions)
+        {
+            long lastValue = -1;
+            bool hasRecovered = false;
+
+            var processor = new Processor(instructions, Common.DayPart.One);
+            processor.OnRecieve += x => hasRecovered = true;
+            processor.OnSend += val => lastValue = val;
+
+            int pointer = 0;
+            while (!hasRecovered)
+                pointer = processor.RunSingleInstruction(pointer);
+
+            return lastValue;
+        }
+
+        static void Main(string[] args)
+        {
+            var instructions = File.ReadAllLines("input.txt");
+
+            int sendCount = 0;
+
+            int pointer1 = 0;
+            var processor1 = new Processor(instructions, Common.DayPart.Two);
+            processor1.GetRegister("p").Value = 0;
+
+            int pointer2 = 0;
+            var processor2 = new Processor(instructions, Common.DayPart.Two);
+            processor2.GetRegister("p").Value = 1;
+
+            processor1.OnSend += val => processor2.InputQueue.Enqueue(val);
+            processor2.OnSend += val => { processor1.InputQueue.Enqueue(val); sendCount++; };
+
+            while (ProcessorStepped(processor1, ref pointer1) || ProcessorStepped(processor2, ref pointer2)) { }
+
+            Console.WriteLine($"Program1 send {sendCount} values");
+        }
+
+        static bool ProcessorStepped(Processor processor, ref int pointer)
+        {
+            var pointerBefore = pointer;
+            pointer = processor.RunSingleInstruction(pointerBefore);
+
+            return pointerBefore != pointer;
+        }
+    }
+}
