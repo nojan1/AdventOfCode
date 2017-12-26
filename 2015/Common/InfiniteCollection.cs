d7ec58d025998e193d3958d687c5d835a9c4f77e
Niklas Hedlund <nojan1989@gmail.com>
2017-12-26 12:13:17 +0200

    Day25

---
+++ 2015/Common/InfiniteCollection.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    public class InfiniteCollectionItem<T>
+    {
+        public int Index { get; set; }
+        public T Value { get; set; }
+    }
+
+    public class InfiniteCollection<T> : IEnumerable<InfiniteCollectionItem<T>>
+    {
+        private Dictionary<int, T> _values = new Dictionary<int, T>();
+
+        public T this[int index]
+        {
+            get
+            {
+                if (_values.TryGetValue(index, out T value))
+                    return value;
+
+                return default(T);
+            }
+            set
+            {
+                _values[index] = value;
+            }
+        }
+
+        public IEnumerator<InfiniteCollectionItem<T>> GetEnumerator()
+        {
+            return _values.Select(x => new InfiniteCollectionItem<T> { Index = x.Key, Value = x.Value }).GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+    }
+}
---
+++ 2017/Day25/Program.cs
@@ -0,0 +1,162 @@
+﻿using Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day25
+{
+    class RuleDefinition
+    {
+        public bool Write { get; set; }
+        public int MoveDirection { get; set; }
+        public string ContinueWith { get; set; }
+    }
+
+    class StateRules
+    {
+        public Dictionary<bool, RuleDefinition> Rules { get; set; }
+    }
+   
+
+    class Program
+    {
+        static Dictionary<string, StateRules> GetRules()
+        {
+            return new Dictionary<string, StateRules>
+            {
+                {"A", new StateRules
+                {
+                    Rules = new Dictionary<bool, RuleDefinition>
+                    {
+                        {false, new RuleDefinition
+                        {
+                            Write = true,
+                            MoveDirection = +1,
+                            ContinueWith = "B"
+                        }},
+                        {true, new RuleDefinition
+                        {
+                            Write = false,
+                            MoveDirection = -1,
+                            ContinueWith = "C"
+                        } }
+                    }
+                } },
+                {"B", new StateRules
+                {
+                    Rules = new Dictionary<bool, RuleDefinition>
+                    {
+                        {false, new RuleDefinition
+                        {
+                            Write = true,
+                            MoveDirection = -1,
+                            ContinueWith = "A"
+                        }},
+                        {true, new RuleDefinition
+                        {
+                            Write = true,
+                            MoveDirection = +1,
+                            ContinueWith = "C"
+                        } }
+                    }
+                } },
+                {"C", new StateRules
+                {
+                    Rules = new Dictionary<bool, RuleDefinition>
+                    {
+                        {false, new RuleDefinition
+                        {
+                            Write = true,
+                            MoveDirection = +1,
+                            ContinueWith = "A"
+                        }},
+                        {true, new RuleDefinition
+                        {
+                            Write = false,
+                            MoveDirection = -1,
+                            ContinueWith = "D"
+                        } }
+                    }
+                } },
+                {"D", new StateRules
+                {
+                    Rules = new Dictionary<bool, RuleDefinition>
+                    {
+                        {false, new RuleDefinition
+                        {
+                            Write = true,
+                            MoveDirection = -1,
+                            ContinueWith = "E"
+                        }},
+                        {true, new RuleDefinition
+                        {
+                            Write = true,
+                            MoveDirection = -1,
+                            ContinueWith = "C"
+                        } }
+                    }
+                } },
+                {"E", new StateRules
+                {
+                    Rules = new Dictionary<bool, RuleDefinition>
+                    {
+                        {false, new RuleDefinition
+                        {
+                            Write = true,
+                            MoveDirection = +1,
+                            ContinueWith = "F"
+                        }},
+                        {true, new RuleDefinition
+                        {
+                            Write = false,
+                            MoveDirection = +1,
+                            ContinueWith = "A"
+                        } }
+                    }
+                } },
+                {"F", new StateRules
+                {
+                    Rules = new Dictionary<bool, RuleDefinition>
+                    {
+                        {false, new RuleDefinition
+                        {
+                            Write = true,
+                            MoveDirection = +1,
+                            ContinueWith = "A"
+                        }},
+                        {true, new RuleDefinition
+                        {
+                            Write = true,
+                            MoveDirection = +1,
+                            ContinueWith = "E"
+                        } }
+                    }
+                } }
+            };
+        }
+
+        static void Main(string[] args)
+        {
+            var numSteps = 12261543;
+            var state = "A";
+            var rules = GetRules();
+
+            var tape = new InfiniteCollection<bool>();
+            int cursor = 0;
+
+            for(int step = 0; step < numSteps; step++)
+            {
+                var rule = rules[state].Rules[tape[cursor]];
+
+                tape[cursor] = rule.Write;
+                cursor += rule.MoveDirection;
+                state = rule.ContinueWith;
+            }
+
+            var numTrue = tape.Count(x => x.Value);
+            Console.WriteLine($"There are {numTrue} positions on");
+        }
+    }
+}
