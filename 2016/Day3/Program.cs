9538d1755c1085dffed43119a9708a0b7fba230b
Niklas Hedlund <nojan1989@gmail.com>
2018-01-04 08:45:27 +0100

    Day3 cleanup

--- 2016/Day3/Program.cs
+++ 2016/Day3/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Common;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -9,42 +10,21 @@ namespace Day3
 {
     class Triangle
     {
-        public Triangle(string[] sides)
-        {
-            if (sides.Length != 3)
-                throw new Exception("Bad input data, doesn't have 3 sides");
-
-            int side1 = Convert.ToInt32(sides[0]);
-            int side2 = Convert.ToInt32(sides[1]);
-            int side3 = Convert.ToInt32(sides[2]);
-
-            if (side3 > side2 && side3 > side1)
-            {
-                Hypotenuse = side3;
-                Side1 = side1;
-                Side2 = side2;
-            }
-            else if (side2 > side3 && side2 > side1)
-            {
-                Hypotenuse = side2;
-                Side1 = side1;
-                Side2 = side3;
-            }
-            else
-            {
-                Hypotenuse = side1;
-                Side1 = side2;
-                Side2 = side3;
-            }
-        }
-
         public int Side1 { get; private set; }
         public int Side2 { get; private set; }
         public int Hypotenuse { get; private set; }
+        public bool IsPossible => Side1 + Side2 > Hypotenuse;
 
-        public bool IsPossible()
+        public Triangle(string[] parts)
         {
-            return Side1 + Side2 > Hypotenuse;
+            var sides = parts
+                .Select(s => Convert.ToInt32(s))
+                .OrderByDescending(x => x)
+                .ToList();
+
+            Hypotenuse = sides[0];
+            Side1 = sides[1];
+            Side2 = sides[2];
         }
     }
 
@@ -52,17 +32,36 @@ namespace Day3
     {
         static void Main(string[] args)
         {
-            var values = File.ReadAllLines("input.txt").Select(l => l.Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries)).ToArray();
-            var triangles = new List<Triangle>();
+            var part = DayPart.Two;
 
-            for(int i = 0; i < values.Length; i += 3)
+            ////
+
+            var values = File.ReadAllLines("input.txt")
+                .Select(l => l.Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries))
+                .ToArray();
+
+            List<Triangle> triangles;
+
+            if(part == DayPart.One)
             {
-                triangles.Add(new Triangle(new string[] { values[i][0], values[i + 1][0], values[i + 2][0] }));
-                triangles.Add(new Triangle(new string[] { values[i][1], values[i + 1][1], values[i + 2][1] }));
-                triangles.Add(new Triangle(new string[] { values[i][2], values[i + 1][2], values[i + 2][2] }));
+                triangles = values
+                    .Select(x => new Triangle(x))
+                    .ToList();
+            }
+            else
+            {
+                
+                triangles = new List<Triangle>();
+
+                for (int i = 0; i < values.Length; i += 3)
+                {
+                    triangles.Add(new Triangle(new string[] { values[i][0], values[i + 1][0], values[i + 2][0] }));
+                    triangles.Add(new Triangle(new string[] { values[i][1], values[i + 1][1], values[i + 2][1] }));
+                    triangles.Add(new Triangle(new string[] { values[i][2], values[i + 1][2], values[i + 2][2] }));
+                }
             }
 
-            var numPossible = triangles.Count(t => t.IsPossible());
+            var numPossible = triangles.Count(t => t.IsPossible);
             Console.WriteLine($"{numPossible} triangles are possible");
         }
     }
