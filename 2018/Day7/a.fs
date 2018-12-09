282c49dd78c6897df42f2b9d4b4afc351a4245f1
Niklas Hedlund <nojan1989@gmail.com>
2018-12-09 20:35:05 +0100

    Missed a file

--- 2018/Day7/a.fs
+++ 2018/Day7/a.fs
@@ -12,29 +12,29 @@ let requirements =
     Array.map parseRequirement
 
 let findSequence (requirements: Requirement[]) =
-    let rec findSequenceInternal (path:string) =
+    let rec findSequenceInternal (path:string) ending =
         let possibleTargets = 
             requirements |>
-            Seq.filter (fun x -> not (path.Contains(x.Target)) && path.Contains(x.Requires)) |>
+            Seq.filter (fun x -> x.Target <> ending && not (path.Contains(x.Target)) && path.Contains(x.Requires)) |>
             Seq.sortBy (fun x -> x.Target) |>
             Seq.toList
 
         match possibleTargets with
         | [] -> path
-        | nextTarget::tail -> findSequenceInternal (path + nextTarget.Target)                               
+        | nextTarget::tail -> findSequenceInternal (path + nextTarget.Target) ending                             
 
     let start = requirements |> Seq.find (fun x -> requirements |> Seq.forall (fun y -> x.Requires <> y.Target))
-    findSequenceInternal start.Requires
-
+    let ending = requirements |> Seq.find (fun x -> requirements |> Seq.forall (fun y -> y.Requires <> x.Target))
+    (findSequenceInternal start.Requires ending.Target) + ending.Target
 let a = findSequence requirements
 
 
 
-let testdata = ["Step C must be finished before step A can begin.";
+let testdata = [|"Step C must be finished before step A can begin.";
 "Step C must be finished before step F can begin.";
 "Step A must be finished before step B can begin.";
 "Step A must be finished before step D can begin.";
 "Step B must be finished before step E can begin.";
 "Step D must be finished before step E can begin.";
-"Step F must be finished before step E can begin."]
-let test = findSequence (testdata |> Seq.map parseRequirement)
+"Step F must be finished before step E can begin."|]
+let test = findSequence (testdata |> Array.map parseRequirement)
