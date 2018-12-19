17ddd25260000813f12e1da6065c545d77a1058a
Niklas Hedlund <nojan1989@gmail.comn>
2018-12-19 12:29:37 +0100

    Completed day 6

--- 2018/Day6/solution.fs
+++ 2018/Day6/solution.fs
@@ -2,9 +2,9 @@
 let distance (x1,y1) (x2,y2) =
     (abs (x1-x2)) + (abs (y1-y2))
 
-let coordinates = 
+let coordinates =
     System.IO.File.ReadAllLines("2018/Day6/input.txt") |>
-    Array.map (fun l -> 
+    Array.map (fun l ->
         let parts = l.Split ','
         (int (parts.[0].Trim()), int (parts.[1].Trim()))
     )
@@ -13,16 +13,13 @@ let minX = coordinates |> Seq.minBy fst |> fst
 let maxX = coordinates |> Seq.maxBy fst |> fst
 let minY = coordinates |> Seq.minBy snd |> snd
 let maxY = coordinates |> Seq.maxBy snd |> snd
-let finiteCoordinates = 
-    coordinates |>
-    Seq.filter (fun (x,y) -> x > minX && x < maxX && y > minY && y < maxY)
 
 let ownerMap =
     seq { for x in minX..maxX do for y in minY..maxY do yield (x,y) } |>
+    Seq.filter (fun (x,y) -> not (Array.contains (x,y) coordinates)) |>
     Seq.map (fun (x,y) ->
-        let distances = 
-            coordinates |> 
-            Seq.filter (fun (x2,y2) -> x <> x2 && y <> y2) |> 
+        let distances =
+            coordinates |>
             Seq.map (fun (x2,y2) -> ((x2,y2), distance (x,y) (x2,y2))) |>
             Seq.sortBy snd |>
             Seq.toList
@@ -30,25 +27,25 @@ let ownerMap =
         if (snd distances.[0]) = (snd distances.[1]) then
             (0, 0)
         else
-            fst distances.[0]              
+            fst distances.[0]
     ) |>
     Seq.toList
 
-let maxSize = 
+let a =
     ownerMap |>
     List.countBy (id) |>
     List.maxBy snd |>
-    snd
+    snd |>
+    (+) 1 //Include the coordinate itself
 
-//6631 to high so is 5372
-let distances = 
-    coordinates |>
-    Seq.map (fun (x1,y1) ->
-        let friends = [
-            for (x2,y2) in coordinates do
-                if x1 <> x2 && y1 <> y2 then
-                    yield distance (x1,y1) (x2,y2)    
-        ]
+let b = 
+    seq { for x in minX..maxX do for y in minY..maxY do yield (x,y) } |>
+    Seq.filter (fun (x,y) ->
+        let totalDistance = 
+            coordinates |>
+            Seq.map (fun (x2,y2) -> distance (x,y) (x2,y2)) |>
+            Seq.sum
 
-        ((x1, y1), friends)
-    )
+        totalDistance < 10000
+    ) |>
+    Seq.length
