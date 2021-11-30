058ce1e8e69da6cf9efea055ffb4c475018d0463
Niklas Hedlund <nojan1989@gmail.com>
2021-11-30 13:43:33 +0100

    Add skeleton in preparation for day1

--- 2021/aoc/src/lib.rs
+++ 2021/aoc/src/lib.rs
@@ -1,8 +1,14 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
-    }
-}
+pub fn read_input(filename: &str) -> Vec<Vec<char>>{
+    fs::read_to_string(filename).unwrap()
+        .lines().collect::<Vec<&str>>().iter()
+        .map(|x| x.chars().collect()).collect::<Vec<Vec<char>>>()
+} 
+
+// #[cfg(test)]
+// mod tests {
+//     #[test]
+//     fn it_works() {
+//         let result = 2 + 2;
+//         assert_eq!(result, 4);
+//     }
+// }
