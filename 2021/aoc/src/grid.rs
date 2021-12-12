4dfcd29c0d189f1bf8cc6ad593e50463eec71655
Niklas Hedlund <noja1989@gmail.com>
2021-12-12 15:20:26 +0100

    Day 11 complete

--- 2021/aoc/src/grid.rs
+++ 2021/aoc/src/grid.rs
@@ -1,27 +1,60 @@
+
+use std::collections::HashMap;
 use std::str::FromStr;
 use std::fmt::Debug;
 
 use crate::read_grid_into;
 
-pub struct Grid<T: FromStr> {
+pub struct Grid<'a, T: FromStr> {
     pub width: usize,
     pub height: usize,
-    items: Vec::<Vec::<GridItem<T>>>
+    items: HashMap::<(usize, usize),GridItem<'a, T>>
 }
 
-pub struct GridItem<T> {
-    x: usize,
-    y: usize,
-    value: T
+pub struct GridItem<'a, T> {
+    pub x: usize,
+    pub y: usize,
+    pub value: &'a T
 }
 
-impl<T: FromStr> Grid<T> where <T as FromStr>::Err: Debug {
+impl<T: FromStr> Grid<'_, T> where <T as FromStr>::Err: Debug {
     pub fn new(filename: &str) {
         let data = read_grid_into::<T>(filename);
 
+        let mut items = HashMap::<(usize, usize), GridItem<T>>::new();
+        for (y,row) in data.iter().enumerate() {
+            for (x, value) in row.iter().enumerate() {
+                items.insert((x,y), GridItem { x, y, value });
+            }
+        }
     }
 
-    pub fn get_neighbours(&self, x: usize, y: usize) -> Option<&[GridItem<T>]>{
-        None
+    pub fn get(&self, x: usize, y: usize) -> Option<&GridItem<T>> {
+        self.items.get(&(x,y))
+    }
+
+    pub fn get_neighbours(&self, x: usize, y: usize, include_diagonal: bool) -> Vec::<&GridItem<T>>{
+        let mut neighbours = Vec::<&GridItem<T>>::new();
+        let x = x as i32;
+        let y = y as i32;
+
+        for (xdiff, ydiff) in [(0,1), (1,1), (1,0), (1,-1), (0,-1),(-1,-1),(-1,0),(-1,1)] {
+            if !include_diagonal && xdiff != 0 && ydiff != 0 {
+                continue;
+            }
+
+            if (xdiff < 0 && x == 0) || (ydiff < 0 && y == 0) || x + xdiff > (self.width as i32) - 1 || y + ydiff > (self.height as i32) - 1 {
+                continue;
+            }
+
+            let lookup_x = (x + xdiff) as usize;
+            let lookup_y = (y + ydiff) as usize;
+
+            if let Some(item) = self.get(lookup_x, lookup_y) {
+                neighbours.push(item);
+            }
+        }
+
+        neighbours
     }
 }
--- 2021/day11/src/main.rs
+++ 2021/day11/src/main.rs
@@ -5,45 +5,87 @@ fn main() {
     println!("Answer to B: {}", part_b("day11/input.txt"));
 }
 
-fn check_neighbours(input: &mut Vec::<Vec::<i64>>, did_flash: &mut Vec::<(usize, usize)>, x: usize, y: usize) {
+fn on_flash(input: &mut Vec::<Vec::<i64>>, did_flash: &mut Vec::<(usize, usize)>, x: usize, y: usize) {
+    if did_flash.contains(&(x,y)) {
+        return;
+    }
+    
+    did_flash.push((x,y));
 
-}
+    let x = x as i32;
+    let y = y as i32;
 
-fn part_a(filename: &str) -> i64{
-    let mut input = aoc::read_grid_into::<i64>(filename);
+    let mut new_flashes = Vec::<(usize, usize)>::new();
 
-    let grid_height = input.len();
-    let grid_width = input[0].len();
+    for (xdiff, ydiff) in [(0,1), (1,1), (1,0), (1,-1), (0,-1),(-1,-1),(-1,0),(-1,1)] {
+        let new_x = x + xdiff;
+        let new_y = y + ydiff;
 
-    for _ in 0..100 {
-        //Steps
-        let mut did_flash = Vec::<(usize, usize)>::new();
+        if new_x >= 0 && new_y >= 0 && new_y < input.len() as i32 && 
+            new_x < input[0].len() as i32 && !did_flash.contains(&(new_x as usize, new_y as usize)) {
 
-        for y in 0..grid_height {
-            for x in 0..grid_width {
-                input[y][x] += 1;
-            }
-        }
+            input[new_y as usize][new_x as usize] += 1;
 
-        for y in 0..grid_height {
-            for x in 0..grid_width {
-                if input[y][x] >= 9 {
-                    //Flashing!!!!
-                    did_flash.push((x,y));
-
-                    check_neighbours(&mut input, &mut did_flash, x, y);
-
-                    input[y][x] = 0;
-                }
+            if input[new_y as usize][new_x as usize] > 9 {
+                new_flashes.push((new_x as usize, new_y as usize));
             }
         }
     }
 
-    0
+    for (flash_x, flash_y) in new_flashes {
+        on_flash(input, did_flash, flash_x, flash_y);
+    }
+
+    input[y as usize][x as usize] = 0;
+}
+
+fn do_step(input: &mut Vec::<Vec::<i64>>) -> i64 {
+    let grid_height = input.len();
+    let grid_width = input[0].len();
+
+    let mut did_flash = Vec::<(usize, usize)>::new();
+
+    for y in 0..grid_height {
+        for x in 0..grid_width {
+            input[y][x] += 1;
+        }
+    }
+
+    for y in 0..grid_height {
+        for x in 0..grid_width {
+            if input[y][x] > 9 {
+                on_flash(input, &mut did_flash, x, y);
+            }
+        }
+    }
+
+    did_flash.len() as i64
+}
+
+fn part_a(filename: &str) -> i64{
+    let mut input = aoc::read_grid_into::<i64>(filename);
+    let mut num_flashes: i64 = 0;
+
+    for _ in 0..100 {
+        num_flashes += do_step(&mut input);
+    }
+
+    num_flashes
 }
 
 fn part_b(filename: &str) -> i64 {
-    0
+    let mut input = aoc::read_grid_into::<i64>(filename);
+    let mut num_steps: i64 = 0;
+
+    loop {
+        num_steps += 1;
+
+        if do_step(&mut input) == 100 {
+            break;
+        }
+    }
+
+    num_steps
 }
 
 #[cfg(test)]
@@ -57,6 +99,6 @@ mod tests {
 
     #[test]
     fn part_b_example_1() {
-        assert_eq!(part_b("input_example.txt"), 0);
+        assert_eq!(part_b("input_example.txt"), 195);
     }
 }
